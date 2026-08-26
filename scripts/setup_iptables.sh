ce5620e135fb3f009ff9927b68f55ef4519f8eb0
erfanbehzadi <146994792+erfanbehzadi@users.noreply.github.com>
2026-08-26 19:15:01 +0330

    Create setup_iptables.sh

---
+++ scripts/setup_iptables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo iptables -F
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p vrrp -j ACCEPT
+sudo netfilter-persistent save
