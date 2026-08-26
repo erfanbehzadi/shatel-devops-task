aa5d1005272199502dbbb643edbc12d650cd9cca
erfanbehzadi <146994792+erfanbehzadi@users.noreply.github.com>
2026-08-26 19:15:16 +0330

    Create sync_authorized_keys.sh

---
+++ scripts/sync_authorized_keys.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rsync -avz -e "ssh -i /home/devops/.ssh/sync_key -o StrictHostKeyChecking=no" \
+  /home/devops/.ssh/authorized_keys \
+  devops@192.168.2.102:/home/devops/.ssh/authorized_keys
