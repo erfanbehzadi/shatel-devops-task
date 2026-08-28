4a299406134d62768476ed2c8035690b9e3b37fa
erfanbehzadi <146994792+erfanbehzadi@users.noreply.github.com>
2026-08-28 14:05:55 +0330

    Update sync_authorized_keys.sh

--- scripts/sync_authorized_keys.sh
+++ scripts/sync_authorized_keys.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-rsync -avz -e "ssh -i /home/devops/.ssh/sync_key -o StrictHostKeyChecking=no" \
-  /home/devops/.ssh/authorized_keys \
-  devops@192.168.2.102:/home/devops/.ssh/authorized_keys
+scp -i /home/devops/.ssh/sync_key -o StrictHostKeyChecking=no /home/devops/.ssh/authorized_keys devops@192.168.2.102:/home/devops/.ssh/authorized_keys
